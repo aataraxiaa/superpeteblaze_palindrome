3667673ffc35af926c87624a5d383d1962742fa1
Pete Smith <peadar81@gmail.com>
2021-01-31 13:34:26 +0000

    Initial commit

---
+++ lib/superpeteblaze_palindrome.rb
@@ -0,0 +1,6 @@
+require "superpeteblaze_palindrome/version"
+
+module SuperpeteblazePalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/superpeteblaze_palindrome/version.rb
@@ -0,0 +1,3 @@
+module SuperpeteblazePalindrome
+  VERSION = "0.1.0"
+end
