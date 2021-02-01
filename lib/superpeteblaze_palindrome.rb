f93b0fae99f1aafa1e0fcd320175711e129c9902
Pete Smith <peadar81@gmail.com>
2021-02-01 15:18:42 +0000

    Refactor, add tests

--- lib/superpeteblaze_palindrome.rb
+++ lib/superpeteblaze_palindrome.rb
@@ -1,6 +1,18 @@
 require "superpeteblaze_palindrome/version"
 
-module SuperpeteblazePalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+# Monkey Patches the String class
+class String
+
+  # Returns true for a palindrome, false otherwise
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
+
 end
